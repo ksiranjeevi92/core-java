265185477f811117454bdffca120ec0a5b780f13
siranjeevi <ksiranjeevi92@gmail.com>
2023-07-26 21:18:28 +0530

    Initial commit

---
+++ src/stringnt/Stringint.java
@@ -0,0 +1,13 @@
+package stringnt;
+
+public class Stringint {
+
+    public static void main(String[] args) {
+
+    String stringInt = "5";
+
+    int total = Integer.parseInt(stringInt) + 20;
+
+    System.out.println(total);
+    }
+}
